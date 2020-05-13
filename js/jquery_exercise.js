0bb46204846b4aae28646cd6a140b368689d047e
Larry Castillo <larrycastillo2009@gmail.com>
2020-05-13 13:37:31 -0500

    update

--- js/jquery_exercise.js
+++ js/jquery_exercise.js
@@ -1,15 +1,42 @@
 "use strict";
 
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
+
+// Jquery exercise 1
+
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+//
+// var contents = $('h1').html();
+// alert(contents);
+//
+// $('.codeup').css('border', '1px solid red ');
+//
+// $('li').css('font-size', '20px');
+//
+// $('p, h1,li').css('background-color', 'yellow');
+
+// ========================================================
+
+// Jquery exerise 2: Mouse Events
+
+
+$('h1').click(function() {
+    $(this).css('background-color','blue');
 });
 
-var contents = $('h1').html();
-alert(contents);
+$('p').dblclick(function(e) {
+    $(this).css('font-size','18px');
+});
 
-$('.codeup').css('border', '1px solid red ');
+$('li').hover(
+    function() {
+        $(this).css('color', 'red');
+    },
+    function() {
+        $(this).css('color', 'black');
+    }
+);
 
-$('li').css('font-size', '20px');
 
-$('p, h1,li').css('background-color', 'yellow');
 
