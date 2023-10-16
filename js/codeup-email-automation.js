99e5e702c8c7c08072c75408903667782e2f8eff
Lola Castiilo <larrycastillo2009@gmail.com>
2023-10-16 16:40:58 -0500

    functions exercise review complete

--- js/codeup-email-automation.js
+++ js/codeup-email-automation.js
@@ -1,13 +1,17 @@
-function myFunction() {
+function studySessionFunction() {
     var ss = SpreadsheetApp.getActiveSpreadsheet();
     var sheet = ss.getSheetByName('Sheet1');
     var dateRange = sheet.getRange("D:D").getValues();
     var emailArr = deliveryStaff();
 
 
-    if(getWeekDayName() === "Thursday"){
+    if(getWeekDayName() === "Wednesday"){
         var today = new Date();
-        var formattedDate = Utilities.formatDate(today, "GMT", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        var currentDay = today.getDate();
+        var twoDaysAway = new Date(today.getFullYear(), today.getMonth(), currentDay + 1);
+        var formattedDate = Utilities.formatDate(twoDaysAway, "GMT", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        console.log(today);
+        console.log(formattedDate);
 
 
         for(var i =0; i< dateRange.length; i++){
@@ -24,7 +28,7 @@ function myFunction() {
                 var index = emailArr.map(function(e) { return e.name; }).indexOf(name)
 
                 console.log(emailArr[index].email);
-
+                return emailArr[index].email
 
 
             }
@@ -35,9 +39,61 @@ function myFunction() {
 
 }
 
-function emailSend(recipient){
-    GmailApp.sendEmail('lola.castillo@codeup.edu','you sent an email!', 'this is the body')
+function emailFunction() {
+    var ss = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = ss.getSheetByName('Sheet1');
+    var dateRange = sheet.getRange("D:D").getValues();
+    var emailArr = deliveryStaff();
+
+
+    if(getWeekDayName() === "Wednesday"){
+        var today = new Date();
+        var currentDay = today.getDate();
+        var twoDaysAway = new Date(today.getFullYear(), today.getMonth(), currentDay + 1);
+        var formattedDate = Utilities.formatDate(twoDaysAway, "GMT", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        console.log(today);
+        console.log(formattedDate);
+
+
+        for(var i =0; i< dateRange.length; i++){
+
+            var formattedDate2 = JSON.stringify(dateRange[i][0], null, 4)
+            var fdate1 = formattedDate.split("T")
+            var fdate2 = formattedDate2.split("T")
+
+            if(fdate2[0].slice(1) === fdate1[0]){
+                console.log("Matched " + fdate2[0].slice(1))
+                var name = sheet.getRange('G' + (i+1)).getValue();
+                console.log(emailArr.length)
+
+                var index = emailArr.map(function(e) { return e.name; }).indexOf(name)
+
+                console.log(emailArr[index].email);
+                return emailArr[index].email
+
+
+            }
+        }
+
+
+    }
+
 }
+
+function emailWeeklySend(recipient){
+    let subject = "Email Prework Support"
+    let body = "Hi there,<br> You are assigned to provide email support for WebDev Pre-work this week.<br> Please monitor your Gmail Inbox<br> throughout the week for email addressed to: webdev-prework@codeup.edu<br> When you respond to the prospective student, be sure to click the “Reply to all” button. If you forget and just Reply to the prospective student, then please Forward your response to webdev-prework@codeup.edu.<br> IMPORTANT 1: Please respond to the prospective student within 24 hours, even if the response is just a request for clarification. If you need ANY help with any part of this support, please post an @Chanel Rodriguez message in the web-development Slack channel.<br> IMPORTANT 2: There are only certain questions that you can answer via email. You can answer any questions that come in for our Teachable pre-work, however, our 45 JS exercises on Glitch can only have questions about the platform (Glitch) answered over email (ex. “What are these errors in my console,” “How do I see my results?“). For any questions about the material (ex. “How do I make a loop?” “How do I create a function?“), you should direct them to the one hour live support session on Thursdays from 4 to 5 via Zoom. The link to the Zoom session is: "+ "<a href='https://us02web.zoom.us/j/87931131822?pwd=SEh3Smo1WkZTQTNKRkxaRDZUOURIQT09'>Zoom Link to Study Session</a>"
+
+    GmailApp.sendEmail(recipient,subject,"", { htmlBody: body })
+};
+
+function studySessionSend(recipient){
+    let subject= "Web Dev Weekly PreWork Study Session";
+    let body= "Hi there,<br> You are assigned to provide Zoom support for WebDev Pre-work this week.<br> The Zoom session is this Thursday from 4p to 5p. If you are assigned in a cohort at that time, please ask to the other cohort instructors to provide coverage during that time. If coverage cannot be provided by your fellow cohort instructors, please email me and/or justin.reich@codeup.edu and we will figure it out.<br> The zoom link is:<br>" + "<a href='https://us02web.zoom.us/j/87931131822?pwd=SEh3Smo1WkZTQTNKRkxaRDZUOURIQT09'>Zoom Link to Study Session</a><br>" + "<br> Thanks!<br> Codeup Notification System └[∵┌]";
+
+    GmailApp.sendEmail(recipient,subject,"", { htmlBody: body })
+};
+
 
 function deliveryStaff(){
     var staff = [
@@ -59,8 +115,7 @@ function deliveryStaff(){
         {name:"Adam", email:"adam.krull@codeup.edu"},
         {name:"Ryan M", email:"ryan.mccall@codeup.edu"},
         {name:"Misty", email:"misty.garcia@codeup.edu"},
-        {name:"Trevor", email:"trevor.mullis@codeup.edu"},
-        {name:"Nikki", email:"nikki.meyer@codeup.edu"},
+        {name:"Trevor", email:"trevor.mullis@codeup.edu"},{name:"Nikki", email:"nikki.meyer@codeup.edu"},
         {name:"Jason", email:"jason.merrell@codeup.edu"},
         {name:"Gonzalo", email:"gonzalo.ovalle@codeup.edu"},
         {name:"Sabrina", email:"sabrina.burch@codeup.edu"},
@@ -123,3 +178,11 @@ function getWeekDayName() {
     // log the output to show the weekday name
     return day;
 }
+
+function run(){
+    studySessionSend(studySessionFunction());
+    emailWeeklySend(emailFunction());
+}
+
+studySessionSend(studySessionFunction());
+emailWeeklySend(emailFunction());
